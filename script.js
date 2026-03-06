7138e25c03ca6be99abb2ac0888dd282c4dc1ebb
Benedito Paiva Brasil Filho <paivarobin16@gmail.com>
2026-03-06 15:48:41 -0300

    Implement close menu on outside click
    Add functionality to close menu when clicking outside of it.

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ menu.style.left = "0px"
 }
 
 }
+
+/* FECHAR MENU AO CLICAR FORA */
+
+document.addEventListener("click", function(event){
+
+let menu = document.getElementById("menu")
+let button = document.querySelector(".menu-icon")
+
+if(
+menu.style.left === "0px" &&
+!menu.contains(event.target) &&
+!button.contains(event.target)
+){
+menu.style.left = "-250px"
+}
+
+})
