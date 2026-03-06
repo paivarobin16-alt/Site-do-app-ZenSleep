dc77a0eba0653db283460a94d26a92ce28a99681
Benedito Paiva Brasil Filho <paivarobin16@gmail.com>
2026-03-06 15:36:52 -0300

    Add toggleMenu function to manage menu display

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMenu(){
+
+let menu = document.getElementById("menu")
+
+if(menu.style.display == "block"){
+
+menu.style.display = "none"
+
+}else{
+
+menu.style.display = "block"
+
+}
+
+}
